4bf5513a441573dcaaeac1a54eb1054031d76619
Tausei722 <kusaki.kei825@gmail.com>
2024-10-22 20:17:04 +0900

    fsgrhtrjytkuy

--- mysite/views.py
+++ mysite/views.py
@@ -5,9 +5,3 @@ def my_custom_page_not_found_view(request, exception):
 
 def my_custom_error_view(request):
     return render(request, '500.html')
-
-def my_custom_permission_denied_view(request):
-    return render(request, '403.html')
-
-def my_custom_bad_request_view(request):
-    return render(request, '400.html')
