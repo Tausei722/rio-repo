bb1760a1ed1322dc342c3ebdf2050cd7626f83a7
Tausei722 <kusaki.kei825@gmail.com>
2024-10-22 13:36:21 +0900

    as error

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'flash.middleware.CustomExceptionMiddleware',
 ]
 MIDDLEWARE_CLASSES = [
     'WhiteNoiseMiddleware',
