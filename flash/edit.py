36b4738b4baabe8b58f6d1198a226645411f03b0
Tausei722 <kusaki.kei825@gmail.com>
2024-10-24 03:00:19 +0900

    download get request

--- flash/edit.py
+++ flash/edit.py
@@ -99,7 +99,7 @@ def download_cloudinary_video(cloudinary_url):
     """
 
     response = requests.get(cloudinary_url, stream=True)
-    save_path = HttpResponse(content_type="mp4")
+    save_path = HttpResponse(content="mp4")
     save_path['Content-Disposition'] = 'attachment; filename="リオ式.mp4"'
     save_path.write()
     with open(MEDIA_ROOT, 'wb') as f:
