ee452c5c19bfa2fb55ce3bb442769f1753844582
Tausei722 <kusaki.kei825@gmail.com>
2025-03-12 05:10:27 +0900

    Update CSRF settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,7 +143,7 @@ CLOUDINARY_STORAGE  = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 cloudinary.config(
-  cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+  cloud_name=os.getenv('CLOUDINARY_NAME'),
   api_key=os.getenv('CLOUDINARY_API_KEY'),
   api_secret=os.getenv('CLOUDINARY_API_SECRET')
 )
