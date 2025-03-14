4f7e73707d81dd4f803a334def709eb7515a820f
Tausei722 <kusaki.kei825@gmail.com>
2025-03-15 01:22:34 +0900

    Update CSRF setting:

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,6 @@ import cloudinary_storage
 from django.shortcuts import render
 import cloudinary.api
 import cloudinary.uploader
-from cloudinary.utils import cloudinary_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
